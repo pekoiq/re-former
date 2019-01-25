4bc71de007abae3fcc9072dfc06ffd0ab38a92f0
Pekoiq <evangarden@gmail.com>
2019-01-24 20:55:29 -0500

    complete re-former

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name,  presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :username, presence: true, length: { minimum: 1 }
+  validates :email, presence: true, length: { minimum: 1 }
+  validates :password, presence: true, length: { minimum: 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
-
+ resources :users, only: [:index, :new, :create, :edit, :update]
 end
